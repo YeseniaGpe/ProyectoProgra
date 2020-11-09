d533c968e1f0e9dd7336b3a1913a542da658cea8
Yesenia Oliva <56052112+YeseniaGpe@users.noreply.github.com>
2020-11-09 17:24:06 -0600

    Arreglé el main e hice el PanelControl que contiene las opciones que usará la tabla

--- src/Vista/Ventana.java
+++ src/Vista/Ventana.java
@@ -1,4 +1,22 @@
 package Vista;
 
-public class Ventana {
+import javax.swing.*;
+import java.awt.*;
+
+public class Ventana extends JFrame {
+
+    public Ventana() {
+        super("Covid-19 en México");
+        setSize(800,430);
+        setResizable(true);
+        setLocation(400,200);
+
+        PanelControl panelIzquierdo  = new PanelControl();
+        PanelGrafica panelDerecho = new PanelGrafica();
+
+        setLayout(new GridLayout(1,2));
+
+        this.add(panelIzquierdo);
+        this.add(panelDerecho);
+    }
 }
