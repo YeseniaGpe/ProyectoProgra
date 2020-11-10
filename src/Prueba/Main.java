f587f737924b834dfe245ba6d8adf7ab362f43c5
uia-poo-2020 <uia.poo.2020@gmail.com>
2020-11-09 19:54:02 -0500

    ConexionDB.java actualizada y probada.

--- src/Prueba/Main.java
+++ src/Prueba/Main.java
@@ -7,7 +7,6 @@ import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
-        ConexionBD conectar = new ConexionBD();
         // Temporal -- Conexion a Base de Datos.
         ConexionBD bdConectar = new ConexionBD();
         Connection conn = bdConectar.getConn();
