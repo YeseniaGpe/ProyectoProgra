5dfb0f8b36b8a04932691a8477337c0cbeed4e9f
uia-poo-2020 <uia.poo.2020@gmail.com>
2020-11-09 19:36:57 -0500

    ConexionDB.java actualizada y probada.

--- src/Prueba/Main.java
+++ src/Prueba/Main.java
@@ -1,9 +1,20 @@
 package Prueba;
 
+import Modelo.ConexionBD;
 import Vista.Ventana;
+
+import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Temporal -- Conexion a Base de Datos.
+        ConexionBD bdConectar = new ConexionBD();
+        Connection conn = bdConectar.getConn();
+        bdConectar.DesconexionBD();
+        conn = bdConectar.getConn();
+        // Termina Temporal
+
 
         Vista.Ventana ventanaPrincipal = new Vista.Ventana();
         ventanaPrincipal.setVisible(true);
