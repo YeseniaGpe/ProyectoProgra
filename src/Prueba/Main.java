f7106f0bc26ea356ef2fdb76a2de8e150623be55
Edgar Luque <edgar.rll@gmail.com>
2020-11-12 18:55:49 -0500

    Hola mundo 3

--- src/Prueba/Main.java
+++ src/Prueba/Main.java
@@ -11,7 +11,7 @@ import java.sql.Connection;
 public class Main {
     public static void main(String[] args) {
         PanelControl ventana = new PanelControl();
-
+        System.out.println("Hola Yesenia");
 
         Vista.Ventana ventanaPrincipal = new Vista.Ventana();
         ventanaPrincipal.setVisible(true);
