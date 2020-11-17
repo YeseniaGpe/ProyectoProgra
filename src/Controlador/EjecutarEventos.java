5ee27cca64678c2605d0ae1ad02ff287fc937111
Yesenia Oliva <56052112+yeseniagpe@users.noreply.github.com>
2020-11-17 18:06:01 -0500

    Con HashMap Lista de Entidades.

--- src/Controlador/EjecutarEventos.java
+++ src/Controlador/EjecutarEventos.java
@@ -2,30 +2,28 @@ package Controlador;
 
 import Modelo.QueryData;
 import Vista.PanelControl;
-import Modelo.Entidad;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class EjecutarEventos implements ActionListener{
     PanelControl ventana = new PanelControl();
-    Entidad listaEstados = new Entidad();
-
-    //Ventana ventana = new Ventana();
 
     public EjecutarEventos(PanelControl ventana){
         this.ventana = ventana;
-        this.listaEstados = listaEstados;
         this.ventana.botonMostrarGrafico.addActionListener(this);
         this.ventana.elegirEstado.addActionListener(this);
     }
 
     public void actionPerformed(ActionEvent e){
-        System.out.println("Boton presionado");
+        //System.out.println("Boton presionado");
         //System.out.println(e.getSource());
 
         if (e.getSource()== ventana.botonMostrarGrafico){
             try {
+                System.out.println(ventana.elegirEstado.getSelectedItem());
+                System.out.println(ventana.sexoFemenino.isSelected());
+                System.out.println(ventana.sexoMasculino.isSelected());
                 System.out.println("Inicia Ejecutar Evento");
                 QueryData queryData = new QueryData();
                 queryData.numberQuery("9", "1", "obesidad");
@@ -33,16 +31,7 @@ public class EjecutarEventos implements ActionListener{
             }catch (Exception ex){
                 System.out.println("Error Ejecutar Evento");
             }
-        }
-        else if(e.getSource() == ventana.elegirEstado) {
-            try {
-                ventana.elegirEstado.removeAllItems();
-                for(int contador=0; contador<listaEstados.listaEntidades.size();contador++) {
-                    ventana.elegirEstado.addItem(listaEstados.listaEntidades.get(contador).getListaEntidades());
-                }
-            }catch (Exception exCombo) {
-                System.out.println("Error al leer los estados desde la base de datos");
-            }
+
         }
     }
 }
--- src/Modelo/Entidad.java
+++ src/Modelo/Entidad.java
@@ -1,15 +1,16 @@
 package Modelo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Entidad {
-    public static  ArrayList<String> listaEntidades;
+    public static HashMap<Integer, String> hmEntidades;
 
     public Entidad(){
-    listaEntidades = new ArrayList<>();
-            listaEntidades.add("Jalisco");
-            listaEntidades.add("Merida");
-            listaEntidades.add("CDMX");
-            listaEntidades.add("Oaxaca");
+        hmEntidades = new HashMap<>();
+            hmEntidades.put(0,"Jalisco");
+            hmEntidades.put(1,"Merida");
+            hmEntidades.put(2,"CDMX");
+            hmEntidades.put(3,"Oaxaca");
     }
 }
