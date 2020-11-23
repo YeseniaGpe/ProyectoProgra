7e746a0395b554add70a70fd2c496d6c0ce516e9
Edgar Luque <edgar.rll@gmail.com>
2020-11-23 15:03:39 -0500

    Devuelve HashMapEdades de clase QueryData

--- src/Modelo/QueryData.java
+++ src/Modelo/QueryData.java
@@ -53,45 +53,57 @@ public class QueryData {
         return numeroQ;
     }
 
-    public void arrayQuery(String entidad, int sexo){
+    public HashMap arrayQuery(String entidad, int sexo) {
 
-        try{
+        HashMap<Integer, String> hashMapEdades = null;
+        try {
             conectarBD = new ConexionBD();
             Connection conn = conectarBD.getConn();
 
+            int claveEntidad = 0;
+
+            for (int i = 1; i < Entidad.hmEntidades.size(); i++) {
+                if (Entidad.hmEntidades.get(i) == entidad) {
+                    System.out.println(Entidad.hmEntidades.get(i));
+                    claveEntidad = i;
+                }
+            }
+
             HashMap<Integer, String> hashMapRangos = new HashMap<>();
-            HashMap<Integer, String> hashMapEdades = new HashMap<>();
+            hashMapEdades = new HashMap<>();
 
-            hashMapRangos.put(1,"20 and 29");
-            hashMapRangos.put(2,"30 and 39");
-            hashMapRangos.put(3,"40 and 49");
-            hashMapRangos.put(4,"50 and 59");
-            hashMapRangos.put(5,"60 and 69");
-            hashMapRangos.put(6,"70 and 100");
+            hashMapRangos.put(1, "20 and 29");
+            hashMapRangos.put(2, "30 and 39");
+            hashMapRangos.put(3, "40 and 49");
+            hashMapRangos.put(4, "50 and 59");
+            hashMapRangos.put(5, "60 and 69");
+            hashMapRangos.put(6, "70 and 100");
 
-            for (int i = 1;i<7;i++){
+            for (int i = 1; i < 7; i++) {
                 String queryDatosCovid = "select count(*) from datoscovid where entidad = "
-                        + entidad + " and sexo = " + sexo +" and edad between "+hashMapRangos.get(i)+" ";
+                        + claveEntidad + " and sexo = " + sexo + " and edad between " + hashMapRangos.get(i);
+
+                System.out.println(queryDatosCovid);
 
                 PreparedStatement qStatement = conn.prepareStatement(queryDatosCovid);
                 ResultSet rs = qStatement.executeQuery();
 
-                while(rs.next()){
+                while (rs.next()) {
                     hashMapEdades.put(i, String.valueOf(rs.getInt(1)));
                 }
             }
 
-            for(int i=1;i<7;i++){
+            for (int i = 1; i < 7; i++) {
                 System.out.println(hashMapEdades.get(i));
             }
 
             conectarBD.DesconexionBD();
 
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Error al ejecutar query.");
             conectarBD.DesconexionBD();
         }
 
-        //return numeroQ;
+        return hashMapEdades;
     }
 }
